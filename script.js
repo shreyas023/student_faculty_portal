07c0770b0572235c98e31fc20c92509f6011313e
SHREYAS ANANDA BAILKAR <91934591+shreyas023@users.noreply.github.com>
2023-10-12 23:09:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");
+var Indicator = document.getElementById("Indicator");
+
+function register() {
+    
+    RegForm.style.transform = "translateX(0px)";
+    LoginForm.style.transform = "translateX(0px)"
+    Indicator.style.transform = "translateX(110px)"
+}
+function login() {
+    
+    RegForm.style.transform = "translateX(300px)";
+    LoginForm.style.transform = "translateX(300px)"
+    Indicator.style.transform = "translateX(0px)"
+}
+
+		
+
+
+			
